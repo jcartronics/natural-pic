4bb275f5954ba0c504dd5b4e5197bdabba4f260d
jcartronics <jcar-95@hotmail.com>
2023-06-30 18:12:15 -0400

    Agrega useContext

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { Navbar } from './components/Navbar';
 import { Home } from './views/Home'
 import { Favoritos } from './views/Favoritos'
 import { useEffect, useState } from 'react';
+import { MyContext } from './context/MyContext';
 
 
 function App() {
@@ -17,29 +18,40 @@ function App() {
   const [fotos, setFotos] = useState({});
 
   const obtenerFotos = async() => {
-    const endpoint = "./fotos.json";
-    const res = await fetch(endpoint);
-    const jsonData = await res.json();
+    try {
+      const endpoint = "./fotos.json";
+      const res = await fetch(endpoint);
+      const jsonData = await res.json();
 
-    setFotos(jsonData);
+      if (!jsonData.photos.length > 0){
+        throw new Error("No hay fotos!")
+      } else {
+        setFotos(jsonData)
+      }
+
+    } catch (error) {
+      console.log("Error al obtener datos", error)
+    }
   }
 
   useEffect(() => {
     obtenerFotos();
   }, [])
-  
+
   return (
     <div className="App">
-      <BrowserRouter>
-      {/* //Componentes */}
-        <Navbar />
+      <MyContext.Provider value={{ fotos, setFotos }}>
+        <BrowserRouter>
+          {/* //Componentes */}
+          <Navbar />
 
-        {/* //Rutas */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favoritos" element={<Favoritos />} />
-        </Routes>
-      </BrowserRouter>
+          {/* //Rutas */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favoritos" element={<Favoritos />} />
+          </Routes>
+        </BrowserRouter>
+      </MyContext.Provider>
     </div>
   );
 }
--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -1,13 +1,21 @@
 
-import React from 'react'
+import React, { useContext } from 'react'
 
 //Importa estilos
 import "../assets/css/galeria.css";
+import { MyContext } from '../context/MyContext';
 
 export const Galeria = () => {
+
+    const {fotos, setFotos} = useContext(MyContext);
+
+    console.log(fotos);
+
   return (
     <div className="galeria grid-columns-5 p-3">
-
+        {fotos?.photos?.map((foto) => 
+            <img key={foto.id} src={foto.src.tiny} alt="" />
+        )}
     </div>
   )
 }
